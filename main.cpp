98052f521c6414832b1a3c3a503bd4e626a0f967
Ian <80492876+TWian1@users.noreply.github.com>
2024-12-17 22:54:21 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,9 @@
 using namespace std;
 
 
+
 // Coordanite Logic
+
 struct floatCoordanite
 {
 	float x,y;
@@ -18,15 +20,26 @@ struct floatCoordanite
 		return out;
 	}
 };
+
+
+
+
+// Unique ID assigning
+
 int idnum = 0;
 
 int getnewid() {
 	idnum++;
 	return idnum - 1;
 }
-// Pixel Creature Class
-class Pixel {
 
+
+
+// Pixel Creature Class
+
+class Pixel {
+	private:
+		SDL_Rect rect;
 	public:
 		int x;
 		int y;
@@ -34,9 +47,11 @@ class Pixel {
 		int energy;
 		bool selection = true;
 		SDL_Color Color = { 255, 255, 255, 255 };
-		SDL_Rect rect;
+
+
 
 		// Initialization Function
+
 		Pixel(int xcoord, int ycoord, int energ) {
 			x = xcoord;
 			y = ycoord;
@@ -44,7 +59,10 @@ class Pixel {
 			id = getnewid();
 		}
 
+
+
 		//Transform And Render to Screen
+
 		void Render(SDL_Renderer* renderer, floatCoordanite Camera, float cameraZoom) {
 			SDL_SetRenderDrawColor(renderer, Color.r, Color.g, Color.b, Color.a);
 			rect.w = cameraZoom+1;
@@ -53,9 +71,12 @@ class Pixel {
 			rect.y = (y - Camera.y)*cameraZoom;
 			SDL_RenderFillRect(renderer, &rect);
 		}
+
+
+
+		//Decisions
+
 		void Update(vector<Pixel>& Pixels) {
-			
-			//Decisions
 
 			int randdir = rand() % 8;
 			if (randdir == 0) {
@@ -82,11 +103,56 @@ class Pixel {
 			
 
 		}
+
+
+
+		// Reproduce Function
+
 		void Reproduce(vector<Pixel>& Pixels) {
 			Pixel newPixel(x + 1-(2*(rand()%2)), y, 15);
 			Pixels.push_back(newPixel);
 		}
 };
+
+
+
+// Food Class
+
+class Food {
+	private:
+		SDL_Rect rect;
+	public:
+		int x;
+		int y;
+		int remaining;
+		int fid;
+
+
+		// Initialization Function
+
+		Food(int xcoord, int ycoord, int remain) {
+			x = xcoord;
+			y = ycoord;
+			remaining = remain;
+		}
+		
+		// Transformation and Rendering
+
+		void Render(SDL_Renderer* renderer, floatCoordanite Camera, float cameraZoom) {
+			SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+			rect.w = cameraZoom + 1;
+			rect.h = cameraZoom + 1;
+			rect.x = (x - Camera.x) * cameraZoom;
+			rect.y = (y - Camera.y) * cameraZoom;
+			SDL_RenderFillRect(renderer, &rect);
+		}
+
+
+};
+
+
+
+//Gets the Pixel ID of a specific coordanite
 
 vector<int> getPixelId(vector<Pixel> Pixels, floatCoordanite mousePosition) {
 	vector<int> out;
@@ -98,11 +164,13 @@ vector<int> getPixelId(vector<Pixel> Pixels, floatCoordanite mousePosition) {
 	return out;
 }
 
+
+
+//Converts the ID of a Pixel into its index in the Pixel list
+
 int indexFromId(int id1, vector<Pixel> Pixels) {
-	//cout << Pixels[0].id << endl;
 	for (int j = 0; j < Pixels.size(); j++) {
 		if (Pixels[j].id == id1) {
-			//cout << Pixels[j].id << "  " << id1 << "   " << j << endl;
 			return j;
 		}
 	}
@@ -121,6 +189,7 @@ int main() {
 
 
 	// Create a Display Bounds Rectangle and check for errors
+
 	SDL_Rect displayBounds;
 	if (SDL_GetDisplayBounds(0, &displayBounds) != 0) {
 		std::cerr << "SDL_GetDisplayBounds failed: " << SDL_GetError() << std::endl;
@@ -128,12 +197,15 @@ int main() {
 		return 1;
 	}
 
+
 	// Set the window scrren height and width to that of the display bounds
+
 	int screen_width = displayBounds.w;
 	int screen_height = displayBounds.h - 60;
 
 
 	// Settings
+
 	bool frameLimitOn = true;
 	int frameLimit = 100;
 
@@ -141,6 +213,7 @@ int main() {
 
 
 	// Create Window and check for errors
+
 	SDL_Window* window = SDL_CreateWindow("help", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screen_width, screen_height, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
 	if (window == nullptr) {
 		cout << "SDL_CreateWindow Error: " << SDL_GetError() << endl;
@@ -150,6 +223,7 @@ int main() {
 
 
 	// Create renderer and check for errors
+
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 	if (renderer == nullptr) {
 		cout << "SDL_CreateRenderer Error: " << SDL_GetError() << endl;
@@ -160,12 +234,17 @@ int main() {
 
 
 	// Create event listener
+
 	SDL_Event e;
 
+
 	// Boolean to run the game loop
+
 	bool gameloop = true;
 
+
 	// Initialize all of the required variables
+
 	unsigned long frameCount = 0;
 	unsigned long lastFPSFrame = 0;
 	long long lastFPSUnixTime = SDL_GetTicks();
@@ -188,7 +267,10 @@ int main() {
 	selectedPixel.selection = false;
 	int FPS = 0;
 	srand(time(NULL));
+	vector<Food> Foods;
 
+
+	//Generates Pixel objects and their attributes 
 
 	for (int xy = 0; xy < 2500; xy++) {
 		if (rand() % 6 == 0) {
@@ -197,10 +279,25 @@ int main() {
 		}
 	}
 
+
+	//Generates Food objects and their attributes
+
+	for (int xy = 0; xy < 250000; xy++) {
+		if (rand() % 20 == 0) {
+			Food genfood(((xy % 500) * 5)-1250, (floor(xy / 500) * 5)-1250, 5);
+			Foods.push_back(genfood);
+		}
+	}
+
+
+
 	// Main Game Loop
+
 	while (gameloop) {
 
+
 		// Event Handler
+
 		while (SDL_PollEvent(&e)) {
 			switch (e.type) {
 
@@ -238,33 +335,55 @@ int main() {
 					}
 					break;
 				}
+
+				// Mouse Scroll Detection
 				case SDL_MOUSEWHEEL:
+					
+					//On mouse scroll update Camera Position and zoom
 					Zoom += (e.wheel.preciseY / 8.0);
 					if (Zoom < 1.0) {
 						Zoom = 1.0;
 					}
-					Camera.x = (Camera.x + mousePos.x / (Zoom - (e.wheel.preciseY / 8.0))) - (mousePos.x / Zoom);
-					Camera.y = (Camera.y + mousePos.y / (Zoom - (e.wheel.preciseY / 8.0))) - (mousePos.y / Zoom);
+					else {
+						Camera.x = (Camera.x + mousePos.x / (Zoom - (e.wheel.preciseY / 8.0))) - (mousePos.x / Zoom);
+						Camera.y = (Camera.y + mousePos.y / (Zoom - (e.wheel.preciseY / 8.0))) - (mousePos.y / Zoom);
+					}
 					break;
+
+				//Mouse Button Down Detection
 				case SDL_MOUSEBUTTONDOWN:
+
+					// LMB Detection
 					if (e.button.button == SDL_BUTTON_LEFT) {
+
+						//On LMB record mouse position
 						mouseDownPos = mousePos;
 					}
 					break;
+
+				//Mouse Button Up Detection
 				case SDL_MOUSEBUTTONUP:
+
+					// LMB Detection
 					if (e.button.button == SDL_BUTTON_LEFT) {
+
+						//Check to see if mouse moved more than 2 pixels during the period it was held down
 						if (abs(mousePos.x - mouseDownPos.x) < 2 && abs(mousePos.y - mouseDownPos.y) < 2) {
+
+							//Figures out the position of the mouse on the grid and finds the pixels on that position
 							mousePosTransformed.x = floor((mousePos.x / Zoom) + Camera.x);
 							mousePosTransformed.y = floor((mousePos.y / Zoom) + Camera.y);
 							selectedPixels = getPixelId(Pixels, mousePosTransformed);
-							if (selectedPixels.size() != 0) { cout << "SELECTED PIXEL/S:" << endl; }
+
+							//Removes Color from previously selected Pixels
 							for (int g = 0; g < Selectedpix.size(); g++) {
 								curpixind = indexFromId(Selectedpix[g], Pixels);
 								if (curpixind != -1) {
 									Pixels[curpixind].Color = { 255, 255, 255, 255 };
 								}
 							}
-							Selectedpix = {};
+
+							//Sets color of selected pixel and records it
 							selectedPixel.selection = false;
 							Selectedpix = {};
 							for (int selectedpixelindex = 0; selectedpixelindex < selectedPixels.size(); selectedpixelindex++) {
@@ -285,6 +404,7 @@ int main() {
 		SDL_RenderClear(renderer);
 		frameCount++;
 
+		//Deletes Pixels with 0 Energy
 		newPixels = Pixels;
 		for (int Pixelindex = Pixels.size() - 1; Pixelindex >= 0; Pixelindex--) {
 			if (Pixels[Pixelindex].energy == 0) {
@@ -293,17 +413,27 @@ int main() {
 			}
 		}
 		Pixels = newPixels;
+
+
+		//Loop that updates and renders each pixel
 		for (int Pixelindex = 0; Pixelindex < Pixels.size(); Pixelindex++) {
-			if (frameCount % updateEvery == -1) {
+			if (frameCount % updateEvery == 0) {
 				Pixels[Pixelindex].Update(Pixels);
 			}
 			Pixels[Pixelindex].Render(renderer, Camera, Zoom);
+		}
+
+		//Loop that renders each food object
+		for (int Foodindex = 0; Foodindex < Foods.size(); Foodindex++) {
+			Foods[Foodindex].Render(renderer, Camera, Zoom);
 		}
 
 		
 
 		// Render the renderer
 		SDL_RenderPresent(renderer);
+
+		//FPS Calculation and Console output calculations and logic
 		if (frameCount % (frameLimit/4) == 0) {
 			FPS = (1000 * (frameLimit/4)) / (SDL_GetTicks() - lastFPSFrame);
 			system("cls");
@@ -318,6 +448,7 @@ int main() {
 			}
 			lastFPSFrame = SDL_GetTicks();
 		}
+
 		// FPS limit logic
 		if (frameLimitOn) {
 			frameTime = SDL_GetTicks() - currentFrameTime;
